8dd21e0fe4befab0ad9d73725c4d546031cee4aa
LiorLin <liorlin12@gmail.com>
2024-11-18 17:36:37 +0200

    Rectangle calculation - user interactive

---
+++ day03/rectangle.py
@@ -0,0 +1,10 @@
+# This is a file for assignment 3
+
+width = int(input("Enter a rectangle's width:"))
+length = int(input("Enter a rectangle's length:"))
+
+area = width*length
+circumference = (2*width)+(2*length)
+
+print (f"The rectangle's area is {area}.")
+print (f"The rectangle's circumference is {circumference}.")
